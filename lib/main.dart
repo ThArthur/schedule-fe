c5586fb47231a72d46ee9f427dee68baf8d5b929
Arthur <arthur.soares@technosoftware.com.br>
2026-03-30 12:19:08 -0300

    Ajuste

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
       title: 'Psique Lounge',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // Definição de uma cor dourada elegante
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFFD4AF37),
           primary: const Color(0xFFD4AF37),
